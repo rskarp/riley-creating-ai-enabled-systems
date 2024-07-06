558814e8af4e5f0b2859bca6afe1200c34165359
rskarp <rkarp1997@gmail.com>
2024-07-06 01:24:09 -0400

    implement save

--- object_detection_system/pipeline.py
+++ object_detection_system/pipeline.py
@@ -1,6 +1,8 @@
 
 
 import cv2
+import os
+import numpy as np
 
 from src.inference import object_detection
 from src.inference import non_maximal_suppression
@@ -13,23 +15,37 @@ class InferenceService:
         self.detector = detector
         self.nms = nms
 
+    def _save(self, frame, filename, objects):
+        path = 'storages/prediction'
+        file = f'{path}/{filename}'
+        # Create folder if it doesn't exist
+        os.makedirs(path, exist_ok=True)
+        # Save image
+        cv2.imwrite(f'{file}.jpg', frame)
+        # Save objects
+        with open(f'{file}.txt', "w") as objs:
+            objs.write('\n'.join(objects))
+
     def detect(self):
         # TODO: Implement this method for your Module 5 Assignment
+        pass
+
 
 if __name__ == "__main__":
     cfg_path = "yolo_resources/yolov4-tiny-logistics_size_416_1.cfg"
     weights_path = "yolo_resources/models/yolov4-tiny-logistics_size_416_1.weights"
     names_path = "yolo_resources/logistics.names"
-    detector = object_detection.YOLOObjectDetector(cfg_path, weights_path, names_path)
+    detector = object_detection.YOLOObjectDetector(
+        cfg_path, weights_path, names_path)
 
     score_threshold = .5
     iou_threshold = .4
-    nms = non_maximal_suppression.NMS(score_threshold,iou_threshold)
+    nms = non_maximal_suppression.NMS(score_threshold, iou_threshold)
 
     video_source = 'udp://127.0.0.1:23000'
     stream = video_processing.VideoProcessing(video_source)
 
-    pipeline = InferenceService(stream, detector, nms)    
+    pipeline = InferenceService(stream, detector, nms)
 
     for detections in pipeline.detect():
         print(detections)
--- object_detection_system/src/inference/video_processing.py
+++ object_detection_system/src/inference/video_processing.py
@@ -1,18 +1,23 @@
 import cv2
+import numpy as np
+
 
 class VideoProcessing:
 
     def __init__(self, udp_url, skip_every_frame=30):
         self.stream_capture = cv2.VideoCapture(udp_url, cv2.CAP_FFMPEG)
+        self.skip_every_frame = skip_every_frame
 
     def resize_image(self, image):
         height, width, _ = image.shape
         if height != self.output_size and width != self.output_size:
             image = cv2.resize(image, (self.output_size, self.output_size))
 
-    def scale_image(self):
-        # TODO: implement this function to scale the image 
-        # (i.e. pixel-wise normalize and standardize).
+    def scale_image(self, frame):
+        # TODO: implement this function to scale the image
+        # (i.e. pixel-wise normalize and standardize per color channel).
+        # Normalize = X-Xmin / Xmax-Xmin
+        # Standardize = X-mean/std
         pass
 
     def capture_udp_stream(self):
@@ -28,22 +33,28 @@ class VideoProcessing:
             print(f"Error: Unable to open UDP stream {udp_url}")
             return
 
+        frame_count = 0
         while True:
             # Read a frame from the UDP stream
             ret, frame = self.stream_capture.read()
-
 
             if not ret:
                 print("Error: Unable to read frame from UDP stream")
                 break
 
-            yield frame
+            # Only process every nth frame
+            if frame_count % self.skip_every_frame == 0:
+                yield frame
+
+            # Increment frame counter
+            frame_count += 1
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-    
+
         self.stream_capture.release()
         cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     udp_url = 'udp://127.0.0.1:23000'  # Replace with your UDP stream URL
